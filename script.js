43106fea2451ac2a56d87a8a3d76922e729812e1
Costaaja <costaaja017@gmail.com>
2026-04-27 21:18:56 +0700

    belajar type data dan juga variable

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-document.getElementById("")
+//writeln itu menulis kaya di dalam body pada html
+document.writeln("Belajar JavaScript");
+document.writeln("</br>");
+
+//tipe data pada js hanya ada 1 
+document.writeln(100);
+document.writeln("</br>");
+document.writeln(100.900);
+document.writeln("</br>");
+
+//tipe data boolean
+document.writeln(true);
+document.writeln("</br>");
+document.writeln(false);
+document.writeln("</br>");
+
+//tipe data string
+document.writeln("hallo kakang");
+document.writeln("</br>");
+
+//tipe data string menambah menggunakan simbol  +
+document.writeln("mas-mas js:  " + "nambah aja mau coba +");
+document.writeln("</br>");
+
+
+//variable
+var nama = "costa";
+document.writeln("hallo saya : ", nama, " dan saya");
+
+//ubah variable
+nama = "sasa";
+document.writeln("hallo saya : ", nama);
+
+//variable let const
+let umur = 17;
+document.writeln("umur saya: ", umur, " Tahun");
+
+//varible const (ini gak bisa diubah nilainya)
+const jurusan = "Jurusan RPL";
+document.writeln("saya : ", jurusan);
